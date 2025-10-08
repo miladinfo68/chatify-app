66ac00c793e6be54e5f611c52a9ee18518955434
miladinfo68 <miladinfo68@gmail.com>
2025-10-08 23:58:36 +0330

    add github action ci xxx

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,15 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // build: {
-  //   outDir: "dist",
-  // },
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://localhost:3000",
-  //       changeOrigin: true,
-  //     },
-  //   },
-  // },
 });
