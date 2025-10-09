4101bb88a0d74e46848698af4008dce598401107
miladinfo68 <miladinfo68@gmail.com>
2025-10-10 01:30:21 +0330

    set build in vite.config file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build:{manifest:true ,outDir:"./dist"},
+  base:"./",
+  root:"./src",
   plugins: [react(), tailwindcss()],
 });
